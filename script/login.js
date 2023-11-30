1f67b31f8d84537441651e5186ee2684e3871f50
Leandro Macedo <leeandromacedo117@gmail.com>
2023-11-30 20:49:09 -0300

    fiz um pull request novamente pois estava faltando alguns arquivos do login... tome cuidado ao fazer pull request, verifique se não houve novos commits

---
+++ script/login.js
@@ -0,0 +1,51 @@
+function clicks(){
+
+const button = document.querySelector(".button")
+
+// Event 
+button.addEventListener("click" , function (e) {
+
+    // pegando valor do input e armazenando
+    function valueInput(){
+        const nameInput = document.querySelector("#name")
+        let context
+        context= nameInput.value 
+        localStorage.setItem("context", context )
+        
+        const email = document.querySelector("#email").value
+        const password = document.querySelector("#password").value
+
+
+        if(context === ""){
+            alert("Digite um nome")
+            e.preventDefault()
+        }
+
+        if(email  === ""){
+            alert("digite um email válido")
+            e.preventDefault()
+        }
+        
+       if(password === ""){
+        alert("Digite uma senha válida")
+        e.preventDefault()
+       }
+   
+    }
+
+    valueInput()
+     
+})
+
+
+}
+
+function boaVindas(){
+    
+    // reutilizando valor do input recebido
+    const contextLocal = localStorage.getItem("context")
+    const boaVindas = document.querySelector("#boa-vindas")
+    boaVindas.textContent = contextLocal
+    
+   
+}
